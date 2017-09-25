20bca3b128b995b340878228c71de0b867eba041
cieter <cjvanwel@hotmail.nl>
2017-09-25 12:03:41 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link href"">
+</head>
+<body>
+	<div id="modal">
+		<div>
+			<img src="" alt=""/>
+			<p>Barroc IT</p>
+		</div>
+		<div>
+			<form action="">
+				<h1>Log in</h1>
+				<label for="username" >Username</label>
+				<input type="text" name="username" ></input>
+				<label for="password" >Password</label>
+				<input type="password" name="password" ></input>
+				<input type="submit" ></input>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
