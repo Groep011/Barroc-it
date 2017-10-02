d276eeff2c788f3b74d037d935abde4a46032c1d
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-10-02 11:25:26 +0200

    header footer

---
+++ barroc/resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="header-top">
+    <div class="container-wide row-spaced">
+        <p>
+            Logged in as: Voor Achter
+        </p>
+        {{--logged in as {{ $user }}--}}
+    </div>
+</div>
+</div>
+</body>
+</html>
---
+++ barroc/resources/views/header.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Barroc-IT</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../public/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+    <div class="header-top">
+        <div class="container-wide space-right">
+            <p>
+                Logged in as: Voor Achter
+            </p>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <ul class="row-spaced">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Optie</a></li>
+                <li><a href="#">Optie</a></li>
+                <li><a href="#">Optie</a></li>
+            </ul>
+        </div>
+    </div>
+
