1f61dfda666e4761d31d17d3c9c1b4d3ffda3938
Sjoerdh95 <sjoerdh.sh@gmail.com>
2017-10-09 11:52:41 +0200

    Auto stash before merge of "master" and "origin/master"

--- barroc/resources/views/tamplates/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="header-top center">
-    <div class="container-wide center">
-        <p>&copy; 2017 Groep-11<p>
-        {{--logged in as {{ $user }}--}}
-    </div>
-</div>
-</div>
-</body>
-</html>
