d77beecabc1e9a41344f330d42fc2dd51c4327b2
Unknown <timoski@outlook.com>
2017-10-10 09:01:13 +0200

    test
    niks verandert

--- barroc/resources/views/home.blade.php
+++ barroc/resources/views/home.blade.php
@@ -1,1 +1,28 @@
-<!doctype html><html lang="en"><head>    <meta charset="UTF-8">    <meta name="viewport"          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">    <meta http-equiv="X-UA-Compatible" content="ie=edge">    <title>Barroc-IT</title>    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />    <link rel="stylesheet" href="https://bootswatch.com/lumen/bootstrap.min.css">    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet"></head><div class="home">    <div class="container">        <div class="login center">            <form action="">                <div class="form-group">                    <label for="">Username</label>                    <input type="text" name="username" class="form-control">                </div>                <div class="form-group">                    <label for="password">Password</label>                    <input type="password" name="password" class="form-control">                </div>            </form>        </div>    </div></div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Barroc-IT</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
+    <link rel="stylesheet" href="https://bootswatch.com/lumen/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+</head>
+<div class="home">
+    <div class="container">
+        <div class="login center">
+            <form action="">
+                <div class="form-group">
+                    <label for="">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
