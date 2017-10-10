db31e2214e4bd67a8337210248224890089c5270
Unknown <timoski@outlook.com>
2017-10-10 20:31:19 +0200

    layout log
    alleen html en css

--- barroc/resources/views/adminPanels/log.blade.php
+++ barroc/resources/views/adminPanels/log.blade.php
@@ -1,2 +1,23 @@
 @extends('layouts.admin')
 
+@section('title')
+    <h1 class='border-title'>Log</h1>
+@endsection
+
+@section('leftBar')
+    <h1 class='border-item-bottom'>1</h1>
+    <h1 class='border-item-bottom'>2</h1>
+    <h1 class='border-item-bottom'>3</h1>
+    <h1 class='border-item-bottom'>4</h1>
+    <h1 class='border-item-bottom'>5</h1>
+    <h1 class='border-item-bottom-none'>6</h1>
+@endsection
+
+@section('main-content')
+    <h1 class='border-item-bottom'>2</h1>
+    <p>heey</p>
+@endsection
+
+@section('scripts')
+    <h2>scripts</h2>
+@endsection
