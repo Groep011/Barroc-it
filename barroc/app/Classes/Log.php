71e5446ceeebef0a9f78233700afa5fd19a8157c
Unknown <timoski@outlook.com>
2017-10-12 10:25:55 +0200

    almost done
    paar puntjes op de i

--- barroc/app/Classes/Log.php
+++ barroc/app/Classes/Log.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Classes;
+namespace App\model\Log;
 
 class Log
 {
@@ -12,17 +13,37 @@ class Log
         parent::__construct($attributes);
     }
 
-    public function meneger()
-    {}
+    public function manager($id_user, $action)
+    {
+        if ($action == "save")
+        {
+            setLogData(CheckSize(CheckPath(CheckExist(getLog()))), $string, getTime());
+        }
+        elseif($action == "print")
+        {
+            return getLogData(CheckSize(CheckPath(CheckExist(getLog()))));
+        }
+    }
 
     public function printLog()
-    {}
+    {
+
+    }
 
     private function makeNewLog()
-    {}
+    {
+        return $path;
+    }
 
     private function getLog()
-    {}
+    {
+        $path = DB::table('logs')->select('path')->where('full', 'N');        
+    }
+
+    private function setLogData($path, $string, $time)
+    {
+
+    }
 
     private function getTime()
     {
@@ -30,5 +51,43 @@ class Log
     }
 
     private function updateLog()
-    {}
+    {
+
+    }
+
+    private function setLogFull($path)
+    {
+
+    }
+
+    private function CheckPath($path)
+    {
+        if (!file_exists($path))
+        {
+            $path = makeNewLog();
+        }
+
+        return $path;
+    }
+
+    private function CheckSize($path)
+    {
+        if (filesize($path) >= 1000000)
+        {
+            setLogFull($path);
+            $path = makeNewLog();
+        }
+
+        return $path;
+    }
+    
+    private function CheckExist($path)
+    {
+        if (!isset($path))
+        {
+            $path = makeNewLog();
+        }
+
+        return $path;
+    }
 }
