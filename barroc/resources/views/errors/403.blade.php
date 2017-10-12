598d67629e957bbe4f6d2639776eecd4e05d2f53
Unknown <timoski@outlook.com>
2017-10-12 11:34:20 +0200

    403

---
+++ barroc/resources/views/errors/403.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>403</title>
+    <link rel="stylesheet" href="https://bootswatch.com/lumen/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/error.css') }}">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="error-template">
+            <h1 class="whiteText">Of Limits</h1>
+            <h2 class="whiteText">403 fobidden place pls.. go back</h2>
+            <div class="whiteText">
+                You shut not be here!! pls.. go back<br>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
